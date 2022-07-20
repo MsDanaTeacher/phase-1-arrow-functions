dd96afe43bd47bdcb230972e614d79d56a55878c
Dana Lobell <danalobell@gmail.com>
2022-07-20 14:09:33 -0400

    commit tree 576b771c86d5254f4822aae1aab916bfd56b32aa

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(num1, num2) {
+    let a = 2000;
+    let b = 100;
+    const solution = (a/b);
+    return solution;
+};
+
+const square = x => x * x;
+
+const add = (a, b) => a + b;
